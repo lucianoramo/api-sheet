c56e9028fbee6e32073feddeb06d143306878f72
Luciano Ramo - Gazai <luciano.ramo76@gmail.com>
2021-07-15 12:21:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ document.getElementById("row-567154182").appendChild(template)
 
 // Default options are marked with *
 fetch(url,{
+    accept: "application/json",
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
     mode: 'no-cors', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
