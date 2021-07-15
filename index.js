c64843c87acd5a0b03c38c35e2d71c56ed669dac
Luciano Ramo - Gazai <luciano.ramo76@gmail.com>
2021-07-15 12:25:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ fetch(url,{
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
     mode: 'no-cors', // no-cors, *cors, same-origin
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-        'Content-Type': 'application/json'
+    //credentials: 'same-origin', // include, *same-origin, omit
+   // headers: {
+      //  'Content-Type': 'application/json'
         // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
+  //  },
     redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', 
+  //  referrerPolicy: 'no-referrer', 
 }).then(response => response.json()).then(data => console.log(data))
 
 
