b2714f58d05d6a53336009037f3b17056c3c7044
Luciano Ramo - Gazai <luciano.ramo76@gmail.com>
2021-07-15 10:56:43 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const template = document.getElementById("template").content;
+const url = 'https://script.google.com/macros/s/AKfycbxkES476KYOYlIrvzn5uKFVbFxUILyddsO1R4fzAhI/dev'
+//const url = 'https://api.github.com/users/defunkt'
+
+
+document.getElementById("row-567154182").appendChild(template)
+
+
+// Default options are marked with *
+fetch(url).then(response => response.json()).then(data => console.log(data))
+
+
+/*
+, {
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    mode: 'no-cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    // Defaultbody: JSON.stringify(data) // body data type must match "Content-Type" header
+}*/
