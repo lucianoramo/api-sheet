972f3c8b0ab17c9c889f131fad1b2da881a40b85
Luciano Ramo - Gazai <luciano.ramo76@gmail.com>
2021-07-15 13:01:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const template = document.getElementById("template").content;
-const url = 'https://script.google.com/macros/s/AKfycbzugO5ZNvgGJrTCqZnChoqD-5SHDxzWdBJr0rttMbsmLozMPYLxe7YnYsYUvgPiOWGd/exec'
+const url = 'https://sheet.best/api/sheets/7952a4c1-c57c-4201-a5f2-da156bd6ea16'
 //const url = 'https://api.github.com/users/defunkt'
 
 
@@ -7,30 +7,7 @@ document.getElementById("row-567154182").appendChild(template)
 
 
 // Default options are marked with *
-fetch(url,{
-    
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'no-cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    //credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-        'Content-Type': 'application/json',
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'accept-encoding': `gzip, deflate, br`,
-        'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
-        'cookie': '_ga = GA1.2.1067080271.1600721016; __exponea_etc__=64b904fd-af12-42b3-bfaf-5cc886656a5c; __exponea_ab_ABtest_Freshchat_202009__=Freshchat_test_202009',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'none',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_16_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36',
-        'x-client-data': 'CJO2yQEIo7bJAQjBtskBCKmdygEIhrXKAQiZtcoBCKzHygEI6cjKAQie+csB'
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer-when-downgrade', 
-}).then(response => response.json()).then(data => console.log(data))
+fetch(url).then(response => response.json()).then(data => console.log(data))
 
 
 /*
